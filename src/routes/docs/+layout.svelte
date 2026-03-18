3c9a51ab002330952790ca95a14bb37e07ff5fa8
Max Farrell <maxffarrell@gmail.com>
2026-03-17 19:34:16 -0500

    sidebar updates

--- src/routes/docs/+layout.svelte
+++ src/routes/docs/+layout.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-	import { SidebarInset, SidebarProvider } from "$lib/registry/ui/sidebar/index";
+	import { SidebarProvider } from "$lib/registry/ui/sidebar/index";
+	import Header from "$lib/components/Header.svelte";
 	import DocSidebar from "$lib/components/docs/DocSidebar.svelte";
-	import DocsHeader from "$lib/components/docs/DocsHeader.svelte";
 
 	let { children } = $props();
 </script>
 
-<SidebarProvider>
-	<DocSidebar />
-	<SidebarInset>
-		<DocsHeader />
-		<main class="container">
-			{@render children()}
-		</main>
-	</SidebarInset>
+<SidebarProvider style="--sidebar-width: 12rem; --sidebar-width-mobile: 24rem;">
+	<div class="flex min-h-screen w-full flex-col">
+		<Header
+			class="bg-background/95 supports-backdrop-filter:bg-background/60 sticky top-0 z-50 border-b backdrop-blur"
+		/>
+
+		<div class="container flex flex-1">
+			<DocSidebar />
+			<main class="w-full flex-1">
+				{@render children()}
+			</main>
+		</div>
+	</div>
 </SidebarProvider>
