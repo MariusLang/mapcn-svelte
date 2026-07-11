02b549757c5177000de5a0badf4a5f9dd0c76530
Max Farrell <maxffarrell@gmail.com>
2026-07-11 16:57:23 -0400

    Revert to TypeScript 6

--- scripts/eslint-ts6.cjs
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-require("./typescript6-require.cjs");
-
-const path = require("node:path");
-
-const eslintPackage = path.dirname(require.resolve("eslint/package.json"));
-require(path.join(eslintPackage, "bin/eslint.js"));
--- scripts/svelte-check-ts6.cjs
+++
@@ -1,4 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-require("./typescript6-require.cjs");
-
-require("svelte-check/bin/svelte-check");
